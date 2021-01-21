072b3d9c8e187274e80d6002c54c62a494e13236
nickgruner <72981300+nickgruner@users.noreply.github.com>
2021-01-21 17:48:19 +0100

    Update Praeferenzen.java
    modelPraeferenzen

--- src/main/java/SpringApplication/model/Praeferenzen.java
+++ src/main/java/SpringApplication/model/Praeferenzen.java
@@ -2,27 +2,56 @@ package SpringApplication.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 public class Praeferenzen {
     @Id
-    private Long id;
+    private long praef_id;
 
-    private String beschreibung;
+    @ManyToMany
+    Set<Kategorie> preafcategory;
 
-    public void setId(Long id) {
-        this.id = id;
+    @ManyToMany
+    Set<Gericht> preafmeal;
+
+    private String name, ernaerungsform;
+
+
+    private Praeferenzen(long id, String name, String ernaerungsform){
+        this.name = name;
+        this.ernaerungsform = ernaerungsform;
+        this.praef_id = id;
     }
 
-    public Long getId() {
-        return id;
+    public Praeferenzen() {}
+    public static Praeferenzen newPraef(long id, String name, String ernaerungsform) {
+        return new Praeferenzen(id, name, ernaerungsform);
+    }
+
+    public void setId(Long id) {
+        this.praef_id = id;
+    }
+
+    public long getId() {
+        return praef_id;
     }
 
     public void setBeschreibung(String beschreibung) {
-        this.beschreibung = beschreibung;
+        this.name = name;
     }
 
     public String getBeschreibung() {
-        return beschreibung;
+        return name;
+    }
+
+    public void setErnaerungsform(String ernaerungsform) {
+        this.ernaerungsform = ernaerungsform;
+    }
+
+    public String getErnaerungsform(){
+        return ernaerungsform;
     }
 }
